dfc40aa7ca524ea12434c81a4acfe90d3773216f
Karan0Patel <karanpatelkp189@gmail.com>
2024-09-30 16:44:29 -0400

    Added: Login Api

--- be-artify/models/userModel.js
+++ be-artify/models/userModel.js
@@ -1,49 +1,45 @@
 const { default: mongoose } = require("mongoose");
 const mongoDb = require("mongoose");
 
-
-
-const userSchema=new mongoDb.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    profilePicture:{
-        type:String,
-        required:false
-    },
-    coverPicture:{
-        type:String,
-        required:false
-    },
-    subscriptionID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Subscription"
-    },
-    subscriptionStartDate:{
-        type:String,
-        required:false
-    },
-    subscriptionEndDate:{
-        type:String,
-        required:false
-    }
-
+const userSchema = new mongoDb.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: false,
+  },
+  coverPicture: {
+    type: String,
+    required: false,
+  },
+  subscriptionID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subscription",
+  },
+  subscriptionStartDate: {
+    type: String,
+    required: false,
+  },
+  subscriptionEndDate: {
+    type: String,
+    required: false,
+  },
 });
 
+const UserData = mongoDb.model("User", userSchema);
 
-const UserData=mongoDb.model('User',userSchema);
-
-module.exports =UserData;
+module.exports = UserData;
--- be-artify/routes/userRoutes.js
+++ be-artify/routes/userRoutes.js
@@ -2,3 +2,7 @@ const Route=require("express").Router()
 const UserController=require('../controllers/userController')
 
 Route.post("/register",UserController.register);
+Route.post("/validate-user", UserController.login);
+
+
+module.exports = Route;
