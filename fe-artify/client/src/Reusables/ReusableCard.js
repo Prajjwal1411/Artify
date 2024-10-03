e28318b447753368f225e63b9c8c747694d382d7
athulpaul30 <athulpaul7@gmail.com>
2024-10-03 10:09:24 -0400

    minoe design changes

--- fe-artify/client/src/Reusables/ReusableCard.js
+++ fe-artify/client/src/Reusables/ReusableCard.js
@@ -9,8 +9,11 @@ const ReusableCard = ({ index, subscription, onClick }) => {
 
   return (
     <div className={`subscription-card ${cardClass}`}>
+      {index === 0 && <div className="popular-choice-banner">Popular Plan</div>}
       <h2>{subscription.subscriptionType}</h2>
       <h3 className="price">${subscription.subscriptionPrice} / month</h3>
+      <hr className="divider" />
+      <h4 className='highlight'>Highlighted Features</h4>
       <ul className="feature">
         {subscription.features && subscription.features.length > 0 ? (
           subscription.features.map((feature, index) => <li key={index}>{feature}</li>)
