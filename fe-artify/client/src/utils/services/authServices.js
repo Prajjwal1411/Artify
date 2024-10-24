75a47ef4e6419eb6b19632b406602179f7ee7abc
Prajjwal Shukla <prajjwal14112000@gmail.com>
2024-10-23 21:10:26 -0400

    Artify 20 | Connect Signup API

--- fe-artify/client/src/utils/services/authServices.js
+++ fe-artify/client/src/utils/services/authServices.js
@@ -9,3 +9,13 @@ export const login = async (formData) => {
         return ;
     }
 };
+
+export const register = async (formData) => {
+    try {
+        const response = await axios.post("http://localhost:8000/register", formData);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching subscriptions:", error);
+        return ;
+    }
+}
