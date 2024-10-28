3862ef66c888b882360dccbce589ec6299d74eb9
Karan0Patel <karanpatelkp189@gmail.com>
2024-10-28 14:35:20 -0400

    Added: popup and highest bid api

--- fe-artify/client/src/App.js
+++ fe-artify/client/src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import EditProfile from './Components/EditProfile';
-import SubscriptionPage from './Components/SubscriptionPage';
-import Login from './Components/Login';
-import Signup from './Components/SignUp';
-import Popuptest from './Components/Popuptest';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditProfile from "./Components/EditProfile";
+import SubscriptionPage from "./Components/SubscriptionPage";
+import Login from "./Components/Login";
+import Signup from "./Components/SignUp";
 function App() {
   return (
     <div className="App">
       <Router>
-       <Routes>
-        <Route path='/' element={<EditProfile/>}/>
-        <Route path='/subscription' element={<SubscriptionPage/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/test-popup' element={<Popuptest />}/>
-
+        <Routes>
+          <Route path="/" element={<EditProfile />} />
+          <Route path="/subscription" element={<SubscriptionPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </div>
