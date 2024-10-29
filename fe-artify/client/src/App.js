b452daf373aeb09c23a2e3fdcff162807fdfdeb4
Karan0Patel <karanpatelkp189@gmail.com>
2024-10-29 11:17:42 -0400

    Added: Pop Up Component

--- fe-artify/client/src/App.js
+++ fe-artify/client/src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import EditProfile from './Components/EditProfile';
-import SubscriptionPage from './Components/SubscriptionPage';
-import Login from './Components/Login';
-import Signup from './Components/SignUp';
-import ProductCard from './Reusables/ProductCard'; // Reusable Product Card Test
-import HomePage from './Components/HomePage';
-import Profile from './Components/Profile'; 
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditProfile from "./Components/EditProfile";
+import SubscriptionPage from "./Components/SubscriptionPage";
+import Login from "./Components/Login";
+import Signup from "./Components/SignUp";
+import ProductCard from "./Reusables/ProductCard"; // Reusable Product Card Test
+import HomePage from "./Components/HomePage";
+import Profile from "./Components/Profile";
+import Popuptest from "./Components/Popuptest";
 
 function App() {
   return (
     <div className="App">
       <Router>
-       <Routes>
-       <Route path="/homepage" element={<HomePage />} />
-        <Route path='/' element={<EditProfile/>}/>
-        <Route path='/subscription' element={<SubscriptionPage/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/productcard' element={<ProductCard />} /> {/* Test Route for reusable cards */}
-         <Route path='/profile' element={<Profile />} />
-        
+        <Routes>
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/" element={<EditProfile />} />
+          <Route path="/subscription" element={<SubscriptionPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/productcard" element={<ProductCard />} />{" "}
+          {/* Test Route for reusable cards */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/test-popup" element={<Popuptest />} />
         </Routes>
       </Router>
     </div>
