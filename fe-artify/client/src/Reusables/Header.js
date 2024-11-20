b9ee634501c0c5eb00679fa89b6758546f55c827
Prajjwal Shukla <prajjwal14112000@gmail.com>
2024-11-20 15:04:21 -0500

    Artify-42 | Add validation to navbar to show only required links

--- fe-artify/client/src/Reusables/Header.js
+++ fe-artify/client/src/Reusables/Header.js
@@ -8,11 +8,16 @@ const Header = () => {
   const [activeLink, setActiveLink] = useState('Home');
 
   const navigate = useNavigate();
+
+  let userID=localStorage.getItem('userID') || null;
+  console.log(userID)
   const handleLinkClick = (link) => {
     setActiveLink(link)
   };
 
   return (
+
+    userID!==null ? 
     <header className="header">
       <img src={logo} alt="Artify Logo" className="logo" onClick={()=>{navigate('/')}}/>
       <div className="vertical-line"></div>
@@ -74,6 +79,43 @@ const Header = () => {
       </div>
       <button className="login-button" onClick={()=>{navigate('/login')}}>Login</button>
     </header>
+
+    :
+    <header className="header">
+    <img src={logo} alt="Artify Logo" className="logo" onClick={()=>{navigate('/')}}/>
+    <div className="vertical-line"></div>
+    <nav className="navigation">
+      <ul>
+        <li>
+          <a 
+            href="#home" 
+            className={activeLink === 'Home' ? 'active' : ''} 
+            onClick={() => handleLinkClick('/')}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+            <a 
+              href="/subscription" 
+              className={activeLink === 'Subscription' ? 'active' : ''} 
+              onClick={() => handleLinkClick('/subscription')}
+            >
+              Subscription
+            </a>
+          </li>
+        
+      </ul>
+    </nav>
+    <div className="search-container">
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search"
+      />
+    </div>
+    <button className="login-button" onClick={()=>{navigate('/login')}}>Login</button>
+  </header>
   );
 };
 
